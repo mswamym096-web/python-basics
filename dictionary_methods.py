991de88da3f2f9ef8ec3ff62563cf540dcf7bfa7
mahadev <mswamym096@gmail.com>
2025-09-28 01:05:33 +0530

    loop

--- dictionary_methods.py
+++ dictionary_methods.py
@@ -19,3 +19,4 @@ print(emp)
 value = emp.pop('ismarried')
 print(emp)
 print(value)
+
---
+++ for_loop.py
@@ -0,0 +1,12 @@
+fruits = ['apple','mango','banana']
+# loopin through each item
+for fruit in fruits:
+    print(fruit)
+
+    # print len of the each item
+    # print(fruit,"length is",len(fruit))
+
+    #printing only those fruits whose len is greater then or equel to 6
+    if len(fruit) >= 6:
+        print(fruit)
+    
