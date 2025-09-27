abac190650e2f4397f65c553e7e3a2122c0663f6
mahadev <mswamym096@gmail.com>
2025-09-27 06:21:33 +0530

    dictionary

---
+++ dictionary_ex.py
@@ -0,0 +1,39 @@
+name = "shiv"
+age = 25
+company = "xyz"
+hobbies = ['kabbadi', 'swimming','singing']
+salary = 10000
+ismarried = True
+
+person = {
+    "name":"shiv",
+    "age": 25,
+    "company":"xyz",
+    "hobbies":['kabbadi', 'swimming','singing'],
+    "salary":1000,
+    "ismarried":True
+
+}
+print(person['name'],person['age']) # extract the values
+
+persons = [{ "name":"shiv",
+    "age": 25,
+    "company":"xyz",
+    "hobbies":['kabbadi', 'swimming','singing'],
+    "salary":1000,
+    "ismarried":True
+
+},
+{
+     "name":"Mahadev",
+    "age": 25,
+    "company":"xyz",
+    "hobbies":['kabbadi', 'swimming','singing'],
+    "salary":1000,
+    "ismarried":True
+}
+
+]
+
+shivinfo,mahadevinfo= persons
+print(mahadevinfo,shivinfo)
